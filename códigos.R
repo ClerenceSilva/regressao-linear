d509e07319a4b1a3c32ba2c87a005c68cb3daaaa
ClerenceSilva <santanavinicius05@gmail.com>
2026-06-14 22:43:25 -0300

    bases 1

--- códigos.R
+++ códigos.R
@@ -1,1 +1,15 @@
-111
+library(tidyverse)
+
+
+base28 <- read_delim(
+  "lancamentos-comerciais-por-distribuidoras 28",
+  delim = ";",                      
+  locale = locale(
+    encoding  = "Latin1",          
+    decimal_mark    = ",",
+    grouping_mark   = "."
+  ),
+  show_col_types = FALSE
+)
+
+glimpse(base28)
